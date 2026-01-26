01cf573db0b6ecaf9c6c4700bab8c4210586ca71
Praneeth Vallabha <praneeth@quantum-ops.com>
2026-01-26 13:13:25 +0530

    chore: add env debug endpoint

---
+++ app/api/_debug/env/route.js
@@ -0,0 +1,13 @@
+// GET /api/_debug/env
+// Returns booleans for required env vars and current node/vercel envs
+
+export async function GET() {
+  const env = process.env;
+  return Response.json({
+    has_NEXT_PUBLIC_SUPABASE_URL: !!env.NEXT_PUBLIC_SUPABASE_URL,
+    has_NEXT_PUBLIC_SUPABASE_ANON_KEY: !!env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    has_SUPABASE_SERVICE_ROLE_KEY: !!env.SUPABASE_SERVICE_ROLE_KEY,
+    node_env: env.NODE_ENV || null,
+    vercel_env: env.VERCEL_ENV || null,
+  });
+}
